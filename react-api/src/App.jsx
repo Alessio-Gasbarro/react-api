aafa195ba68f331d83a2a84839d755315e7c162f
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-13 16:26:38 +0200

    [UPD] Creating Cards and imported to App

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 const actorEndPoint = 'https://lanciweb.github.io/demo/api/actors/'
 const actressEndpoint = 'https://lanciweb.github.io/demo/api/actresses/'
+import Cards from './components/Cards'
 
 function App() {
 
