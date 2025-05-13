c4fad47fb8093b745a52cfa5cf0d78b73c239eb8
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-13 16:11:15 +0200

    [UPD] Created const, imported Api and created Cards.jsx

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -1,10 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+const actorEndPoint = 'https://lanciweb.github.io/demo/api/actors/'
+const actressEndpoint = 'https://lanciweb.github.io/demo/api/actresses/'
 
 function App() {
 
+  //CONST Attori
+  const [actors, setActors] = useState([]);
+  const [actresses, setActresses] = useState([]);
+
+  //Attori Array
+  const fetchActors = () => {
+    axios.get(actorEndPoint).then((response) => {
+      setActors(response.data)
+    })
+  }
+
+  //Attrici Array
+  const fetchActresses = () => {
+    axios.get(actressEndpoint).then((response) => {
+      setActresses(response.data)
+    })
+  }
+
+  useEffect(() => {
+    fetchActors()
+    fetchActresses()
+  }, [])
+
   return (
     <>
-
+      <h1>Hello Boolean!</h1>
     </>
   )
 }
